5b1b390b4c876dd945b798683a86d91eda211347
krystianbrynski <krystian.brynski@student.put.poznan.pl>
2024-07-08 14:50:55 +0200

    Added config with paths to test and train data

--- project/src/main.py
+++ project/src/main.py
@@ -1,4 +1,17 @@
+import click
+import yaml
+
+
+@click.command()
+@click.option('--config', '-c', default='../config.yaml', help='Path to the configuration file')
 def run_pipeline(config) -> None:
+    with open(config, 'r') as file:
+        config_data = yaml.safe_load(file)
+
+        train_images_dir = config_data.get('images_train')
+        test_images_dir = config_data.get('images_test')
+        train_masks_dir = config_data.get('masks_train')
+        test_masks_dir = config_data.get('masks_test')
 
 
 if __name__ == "__main__":
